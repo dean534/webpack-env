b29b166a20c5804dc39adafd75061ceae0c05240
dean534 <d810218@gmail.com>
2019-05-13 14:00:18 +0800

    創建

---
+++ src/index.js
@@ -0,0 +1,5 @@
+console.log('Hi, team!');
+
+console.log('hello');
+
+console.log('world');
---
+++ webpack.config.js
@@ -0,0 +1,58 @@
+const path = require('path');
+// plugins for creating new "index.html" and replace the original one
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+// clean old file in dist
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+
+module.exports = {
+    mode: 'development',
+    devtool: 'inline-source-map',
+    devServer: {
+        contentBase: './dist'
+    },
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js',
+        publicPath: '/'
+    }, plugins: [
+        new CleanWebpackPlugin(),
+        new HtmlWebpackPlugin({
+            title: '管理输出'
+        })
+    ],
+    module: {
+        rules: [{
+            test: /\.css$/,
+            use: [
+                'style-loader',
+                'css-loader'
+            ]
+        },
+        {
+            test: /\.(png|svg|jpg|gif)$/,
+            use: [
+                'file-loader'
+            ]
+        },
+        {
+            test: /\.(woff|woff2|eot|ttf|otf)$/,
+            use: [
+                'file-loader'
+            ]
+        },
+        {
+            test: /\.(csv|tsv)$/,
+            use: [
+                'csv-loader'
+            ]
+        },
+        {
+            test: /\.xml$/,
+            use: [
+                'xml-loader'
+            ]
+        }
+        ]
+    }
+}
