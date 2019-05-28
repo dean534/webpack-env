3e17b6a7a718d4497e9aa0ef8d28b2b2bd988053
klaus <klaus@toppath.tw>
2019-05-28 21:36:03 +0800

    安裝boostrape，定位為不用框架

--- src/index.js
+++ src/index.js
@@ -1,2 +1,5 @@
-import {test} from './test';
-console.log(test);
+const app = document.getElementById('app');
+const text = document.createElement('h1');
+console.log(text);
+text.innerHTML = 'hello world!小廢物們，我準備統治天下了';
+app.appendChild(text);
--- webpack.config.js
+++ webpack.config.js
@@ -15,54 +15,60 @@ module.exports = {
         path: path.resolve(__dirname, 'dist'),
         filename: 'bundle.js',
         publicPath: '/'
-    }, plugins: [
+    },
+    plugins: [
         new CleanWebpackPlugin(),
         new HtmlWebpackPlugin({
-            title: '管理输出'
+            title: '管理输出',
+            template: 'src/index.html'
         })
     ],
     module: {
         rules: [{
-            test: /\.css$/,
-            use: [
-                'style-loader',
-                'css-loader'
-            ]
-        },
-        {
-            test: /\.(png|svg|jpg|gif)$/,
-            use: [
-                'file-loader'
-            ]
-        },
-        {
-            test: /\.(woff|woff2|eot|ttf|otf)$/,
-            use: [
-                'file-loader'
-            ]
-        },
-        {
-            test: /\.(csv|tsv)$/,
-            use: [
-                'csv-loader'
-            ]
-        },
-        {
-            test: /\.xml$/,
-            use: [
-                'xml-loader'
-            ]
-        },
-        {
-            test: /\.m?js$/,
-            exclude: /(node_modules|bower_components)/,
-            use: {
-              loader: 'babel-loader',
-              options: {
-                presets: ['@babel/preset-env']
-              }
+                test: /\.css$/,
+                use: [
+                    'style-loader',
+                    'css-loader'
+                ]
+            },
+            {
+                test: /\.(png|svg|jpg|gif)$/,
+                use: [
+                    'file-loader'
+                ]
+            },
+            {
+                test: /\.(woff|woff2|eot|ttf|otf)$/,
+                use: [
+                    'file-loader'
+                ]
+            },
+            {
+                test: /\.(csv|tsv)$/,
+                use: [
+                    'csv-loader'
+                ]
+            },
+            {
+                test: /\.xml$/,
+                use: [
+                    'xml-loader'
+                ]
+            },
+            {
+                enforce: "pre",
+                test: /\.js$/,
+                exclude: /(node_modules|bower_components)/,
+                loader: "eslint-loader",
+                options:{
+                    fix: true 
+                }
+            },
+            {
+                test: /\.js$/,
+                exclude: /(node_modules|bower_components)/,
+                loader: "babel-loader"
             }
-          }
         ]
     }
 }
