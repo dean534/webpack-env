dfb7cf5862a805a55cc56ccff7443228467d4f48
klaus <klaus@toppath.tw>
2019-05-28 23:55:55 +0800

    需要再試試sass-loader

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
+import './scss/helper/all.scss';
+
 const app = document.getElementById('app');
 const text = document.createElement('h1');
 console.log(text);
-text.innerHTML = 'hello world!小廢物們，我準備統治天下了';
+text.innerHTML = 'hello world!';
 app.appendChild(text);
